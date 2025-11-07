3873b8927655d71249ceb104d781b509911851c0
Martin Goñi <screadores122@gmail.com>
2025-11-06 21:06:11 -0300

    Added exceptions and memory allocation

--- kernel.c
+++ kernel.c
@@ -2,8 +2,12 @@
 #include "common.h"
 
 //Get linker symbols, this gets us the address of those symbols.
-extern char __bss[], __bss_end[], __stack_top[];
+extern char __bss[], __bss_end[], __stack_top[], __free_ram[], __free_ram_end[];
 
+
+/*
+    SYSCALLS
+*/
 //Perform ECALL to OpenSBI. Transfers control between the SEE and the supervisor.
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
                        long arg5, long fid, long eid) {
@@ -29,14 +33,138 @@ void putchar(const char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
 }
 
-void kernel_main(void) {
-    printf("\n\nHello %s\n", "World!");
-    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
-    for (;;) {
-        //Halt, by waiting for an interruption that will never come.
-        __asm__ __volatile__("wfi");
+/*
+    ERROR HANDLING
+*/
+__attribute__((naked)) //Don't generate unnecessary code before and after function.
+__attribute__((aligned(4)))
+void kernel_entry(void) {
+    __asm__ __volatile__(
+        "csrw sscratch, sp\n" //Store stack pointer in sscratch, the supervisor scratch register.
+        "addi sp, sp, -4 * 31\n" //Allocate space in stack and store all registers.
+        "sw ra,  4 * 0(sp)\n"
+        "sw gp,  4 * 1(sp)\n"
+        "sw tp,  4 * 2(sp)\n"
+        "sw t0,  4 * 3(sp)\n"
+        "sw t1,  4 * 4(sp)\n"
+        "sw t2,  4 * 5(sp)\n"
+        "sw t3,  4 * 6(sp)\n"
+        "sw t4,  4 * 7(sp)\n"
+        "sw t5,  4 * 8(sp)\n"
+        "sw t6,  4 * 9(sp)\n"
+        "sw a0,  4 * 10(sp)\n"
+        "sw a1,  4 * 11(sp)\n"
+        "sw a2,  4 * 12(sp)\n"
+        "sw a3,  4 * 13(sp)\n"
+        "sw a4,  4 * 14(sp)\n"
+        "sw a5,  4 * 15(sp)\n"
+        "sw a6,  4 * 16(sp)\n"
+        "sw a7,  4 * 17(sp)\n"
+        "sw s0,  4 * 18(sp)\n"
+        "sw s1,  4 * 19(sp)\n"
+        "sw s2,  4 * 20(sp)\n"
+        "sw s3,  4 * 21(sp)\n"
+        "sw s4,  4 * 22(sp)\n"
+        "sw s5,  4 * 23(sp)\n"
+        "sw s6,  4 * 24(sp)\n"
+        "sw s7,  4 * 25(sp)\n"
+        "sw s8,  4 * 26(sp)\n"
+        "sw s9,  4 * 27(sp)\n"
+        "sw s10, 4 * 28(sp)\n"
+        "sw s11, 4 * 29(sp)\n"
+
+        "csrr a0, sscratch\n" 
+        "sw a0, 4 * 30(sp)\n"
+
+        "mv a0, sp\n"
+        "call handle_trap\n" //Store SP in a0 and call trap handler.
+
+        "lw ra,  4 * 0(sp)\n"
+        "lw gp,  4 * 1(sp)\n"
+        "lw tp,  4 * 2(sp)\n"
+        "lw t0,  4 * 3(sp)\n"
+        "lw t1,  4 * 4(sp)\n"
+        "lw t2,  4 * 5(sp)\n"
+        "lw t3,  4 * 6(sp)\n"
+        "lw t4,  4 * 7(sp)\n"
+        "lw t5,  4 * 8(sp)\n"
+        "lw t6,  4 * 9(sp)\n"
+        "lw a0,  4 * 10(sp)\n"
+        "lw a1,  4 * 11(sp)\n"
+        "lw a2,  4 * 12(sp)\n"
+        "lw a3,  4 * 13(sp)\n"
+        "lw a4,  4 * 14(sp)\n"
+        "lw a5,  4 * 15(sp)\n"
+        "lw a6,  4 * 16(sp)\n"
+        "lw a7,  4 * 17(sp)\n"
+        "lw s0,  4 * 18(sp)\n"
+        "lw s1,  4 * 19(sp)\n"
+        "lw s2,  4 * 20(sp)\n"
+        "lw s3,  4 * 21(sp)\n"
+        "lw s4,  4 * 22(sp)\n"
+        "lw s5,  4 * 23(sp)\n"
+        "lw s6,  4 * 24(sp)\n"
+        "lw s7,  4 * 25(sp)\n"
+        "lw s8,  4 * 26(sp)\n"
+        "lw s9,  4 * 27(sp)\n"
+        "lw s10, 4 * 28(sp)\n"
+        "lw s11, 4 * 29(sp)\n"
+        "lw sp,  4 * 30(sp)\n"
+        "sret\n"
+    );
+}
+
+void handle_trap(struct trap_frame *f) {
+    uint32_t scause = READ_CSR(scause);
+    uint32_t stval = READ_CSR(stval);
+    uint32_t user_pc = READ_CSR(sepc);
+
+    PANIC("Unexpected trap scause: %x, stval: %x, spec: %x\n", scause, stval, user_pc);
+}
+
+
+/*
+    MEMORY MANAGMENT
+*/
+paddr_t alloc_pages(uint32_t n) {
+    //Initial value for "next_paddr".
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    //Move allocation pointer.
+    next_paddr += n * PAGE_SIZE;
+
+    printf("Na: %x - Fre: %x\n", next_paddr, (paddr_t) __free_ram_end);
+
+    if (next_paddr > (paddr_t) __free_ram_end)
+        PANIC("Out of memory!");
+
+    //Clear allocated memory.
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
+
+/*
+    KERNEL ENTRY POINT
+*/
+void kernel_main(void) {
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+    //Tell the CPU where the exception handler is located.
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+    /*paddr_t a0 = alloc_pages(5);
+    paddr_t a1 = alloc_pages(2);
+
+    printf("Addr0: %x\n", a0);
+    printf("Addr1: %x\n", a1);*/
+
+    for (int i = 0; i < 20000; ++i)
+    {
+        alloc_pages(1);
     }
+    
+    PANIC("Booted");
 }
 
 __attribute__ ((section(".text.boot"))) //Place instruction at ".text.boot", address used by OpenSBI.
